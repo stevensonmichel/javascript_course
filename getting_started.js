c122cc11314d91d437a7828e05196e2fda0defa6
Stevenson <michels@berea.edu>
2024-04-19 10:20:59 -0400

    Second Commit

---
+++ getting_started.js
@@ -0,0 +1,38 @@
+
+// FUNCTIONS DEFINITIONS
+// function sum(a, b) {
+//     return a + b;
+// }
+
+
+// function product(a, b) {
+//     return a * b;
+// }
+
+// console.log(sum(13, 45));
+
+// const summation = sum(18, 9);
+// const production = product(12, 4);
+// console.log(summation, production);
+
+
+// ANONYMOUS FUNCTION
+// setTimeout(function() {
+//     console.log("You rock really like the Rock");
+// }, 3000)
+
+
+
+
+// ARROW FUNCTION
+// setTimeout(() => console.log("You rock bro"), 3000);
+
+const bereaCollege = {
+    student: "Stevenson Michel",
+    staff: "Brian Ramsey",
+    "^-jumpman": "Drake & Future"
+}
+
+console.log(bereaCollege);
+
+console.log(bereaCollege.staff);
