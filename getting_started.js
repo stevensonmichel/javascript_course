d59dfeeab753be71eaff48b7348d575aba9b08d2
Stevenson <michels@berea.edu>
2024-04-19 15:55:32 -0400

    Added functions, conditionals, and AND/OR statements

--- getting_started.js
+++ getting_started.js
@@ -1,9 +1,8 @@
 
-// FUNCTIONS DEFINITIONS
+// 1- FUNCTION DEFINITION
 // function sum(a, b) {
 //     return a + b;
 // }
-
 
 // function product(a, b) {
 //     return a * b;
@@ -16,7 +15,7 @@
 // console.log(summation, production);
 
 
-// ANONYMOUS FUNCTION
+// 2- ANONYMOUS FUNCTION DEFINITION
 // setTimeout(function() {
 //     console.log("You rock really like the Rock");
 // }, 3000)
@@ -24,15 +23,79 @@
 
 
 
-// ARROW FUNCTION
+// 3- ARROW FUNCTION DEFINITION
 // setTimeout(() => console.log("You rock bro"), 3000);
 
-const bereaCollege = {
-    student: "Stevenson Michel",
-    staff: "Brian Ramsey",
-    "^-jumpman": "Drake & Future"
-}
 
-console.log(bereaCollege);
 
-console.log(bereaCollege.staff);
+
+// 4- OBJECT DEFINITION
+// const bereaCollege = {
+//     student: "Stevenson Michel",
+//     staff: "Brian Ramsey",
+//     "^-jumpman": "Drake & Future"
+// }
+
+// console.log(bereaCollege);
+
+// console.log(bereaCollege.staff);
+
+
+
+
+// 5- CLASS DEFINITION
+// class BereaCollege {
+//     constructor(student, staff, isActive) {
+//         this.student = student;
+//         this.staff = staff;
+//         this.isActive = isActive;
+//     }
+
+//     goesToSchool() {
+//         console.log("My name is " + this.student + " and I am an active student " + this.isActive)
+//     }
+// }
+
+// const mySituation = new BereaCollege("Stevenson Michel", "Nope", true);
+
+// mySituation.goesToSchool();
+
+
+
+
+// 6- CONDITIONAL STATEMENTS
+// let numOfPlayers = 34;
+
+// if (numOfPlayers < 23) {
+//     console.log("More players are needed for the team")
+// }
+// else if (numOfPlayers > 23) {
+//     console.log("There are too many players in the team")
+// }
+// else {
+//     console.log("We have our team")
+// }
+
+
+
+
+
+
+
+
+// 7- AND and OR STATEMENTS
+// const school = {
+//     name: "College Savio",
+//     isCatholic: true,
+//     numOfStudents: 1500,
+//     hasAlumniNetwork: false,
+//     bestAlumni: "Stevenson Michel"
+// }
+
+
+// if (school.name != "College Dominique Savio" && school.isCatholic) {
+//     console.log("That is the right school")
+// }
+// else {
+//     console.log("Do they have alumni network " + school.hasAlumniNetwork + " but the best alumni is " + school.bestAlumni)
+// }
